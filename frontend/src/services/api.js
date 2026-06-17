3d73b009cfd52f988f0dfbdd2fc5849e5313e843
swathi kiranmai <swathikiranmai@swathis-MacBook-Air-2.local>
2026-06-18 01:05:38 +0530

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'baseURL: 'https://book-a-doctor-eoiy.onrender.com/api'',
   headers: {
     'Content-Type': 'application/json',
   },
